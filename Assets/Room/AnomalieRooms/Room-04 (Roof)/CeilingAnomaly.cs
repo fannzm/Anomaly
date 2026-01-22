b33639af5860dbc57bdb5bc4bfc02e9a5137d307
philtongee <fhb231471@fh-vie.ac.at>
2026-01-22 12:17:19 +0100

    Added Anomalies
    - Ceiling Anomaly
    - Clock Anomaly
    - Radio Anomaly

---
+++ Assets/Room/AnomalieRooms/Room-04 (Roof)/CeilingAnomaly.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CeilingAnomaly : MonoBehaviour
+{
+    public GameManager gameManager;
+    public float sinkSpeed = 0.15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, -sinkSpeed * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (gameManager != null)
+            {
+                gameManager.WrongChoice(); 
+            }
+        }
+    }
+}
