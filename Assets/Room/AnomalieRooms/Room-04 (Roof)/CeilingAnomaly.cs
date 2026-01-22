dcd35f659091456f82efc4539155aa8c0a5c23b0
philtongee <fhb231471@fh-vie.ac.at>
2026-01-22 22:48:42 +0100

    few changes

--- Assets/Room/AnomalieRooms/Room-04 (Roof)/CeilingAnomaly.cs
+++ Assets/Room/AnomalieRooms/Room-04 (Roof)/CeilingAnomaly.cs
@@ -10,7 +10,7 @@ public class CeilingAnomaly : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = Object.FindFirstObjectByType<GameManager>();
     }
 
     // Update is called once per frame
