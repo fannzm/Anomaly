6fa1d370a4519a6d66c263db2b4fee71f66ff28f
eliasmiksch <elias.miksch@gmail.com>
2026-01-24 19:20:39 +0100

    NewRooms+MainMenue+BetterCollision+SceneManager

--- Assets/MainMenue/MainMenueLogic.cs
+++ Assets/MainMenue/MainMenueLogic.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class MainMenueLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
+    VideoPlayer videoPlayer;
+    public VideoClip GetNewAi;
+    float videoTime;
+
+    public GameObject MainScreenCanva;
+   
+
+
     void Start()
     {
-        
+        MainScreenCanva.SetActive(true);
+       
+        videoPlayer = GameObject.Find("VideoPlayer").GetComponent<VideoPlayer>();
     }
 
     // Update is called once per frame
@@ -15,4 +26,37 @@ public class MainMenueLogic : MonoBehaviour
     {
         
     }
+
+    public void StarGame()
+    {
+
+        MainScreenCanva.SetActive(false);
+        
+        videoPlayer.clip = GetNewAi;
+        videoPlayer.Play();
+        videoTime = (float)videoPlayer.length;
+        Invoke("LoadMainGame", videoTime);
+
+    }
+
+    public void LoadMainGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void Controls()
+    {
+
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
+
+
+
+
+
 }
---
+++ Assets/Room/AnomalieRooms/Room-E03 (Lava Room)/LavaScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEditor.FilePathAttribute;
+
+public class LavaScript : MonoBehaviour
+{
+
+
+    public GameObject Respawnpoint;
+    public Rigidbody Player;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player").GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+            Player = GameObject.Find("Player").GetComponent<Rigidbody>();
+        
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("CollisionDetected");
+
+        if (other.gameObject.tag == "Player")
+        {
+            print("Lava HIT");
+            
+            Player.GetComponent<CharacterController>().enabled = false;
+            Player.transform.position = new Vector3(Respawnpoint.transform.position.x, Respawnpoint.transform.position.y, Respawnpoint.transform.position.z);
+            Player.GetComponent<CharacterController>().enabled = true;
+        }
+    }
+
+
+
+}
